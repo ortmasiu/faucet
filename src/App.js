15f62cb89b6701e8f53d14c7b344bd3059ddb1b2
ortmasiu <ortmasiu@gmail.com>
2024-01-07 00:35:06 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
 
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import Web3 from 'web3';
 
 function App() {
+  const [web3Api, setWeb3Api] = useState({
+    provider: null, 
+    web3: null
+  })
+
   useEffect(() => {
     const loadProvider = async() => {
       // with metamask we have an access to window.ethereum & to window.web3
       // metamask injects a global API into the website
       // this API allows website to request users, accounts and read data to the blockchain
       // sign messales and transactions
-      console.log(window.web3)
-      console.log(window.ethereum)
+      let provider = null;
+
+      if (window.ethereum) {
+        provider = window.ethereum;
+      } else if (window.web3) {
+        provider = window.web3.currentProvider;
+      } else if (!process.env.production) {
+        provider = new Web3.providers.HttpProvider("http://localhost:7545")
+      }
     }
+
+    setWeb3Api({
+      web3: new Web3(provider),
+      provider;
+    })
 
     loadProvider()
   }, [])
