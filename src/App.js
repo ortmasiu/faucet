85947216c2695325671f42a20d04a10a5e1013c2
ortmasiu <ortmasiu@gmail.com>
2024-01-07 01:12:41 +0200

    get account from web3API

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
     web3: null
   })
 
+  const [account, setAccount] = useState(null)
+
   useEffect(() => {
     const loadProvider = async() => {
       // with metamask we have an access to window.ethereum & to window.web3
@@ -19,37 +21,51 @@ function App() {
 
       if (window.ethereum) {
         provider = window.ethereum;
+
+        try {
+          await provider.enable();
+        } catch (e) {
+          console.error("User denied accounts access!")
+        }
+
       } else if (window.web3) {
         provider = window.web3.currentProvider;
       } else if (!process.env.production) {
         provider = new Web3.providers.HttpProvider("http://localhost:7545")
       }
-    }
 
-    setWeb3Api({
-      web3: new Web3(provider),
-      provider;
-    })
+      setWeb3Api({
+        web3: new Web3(provider),
+        provider
+      })
+    }
 
     loadProvider()
   }, [])
+
+  
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts();
+      setAccount(accounts[0]);
+    }
+
+    web3Api.web3 && getAccount();
+  }, [web3Api.web3])
 
   return (
     <>
       <div className="faucet-wrapper">
         <div className="faucet">
+          <span>
+            <strong>Account: </strong>
+          </span>
+          <h1>
+            { account ? account : "not connected" }
+          </h1>
           <div className="balance-view is-size-2">
             Current Balance: <strong>10</strong> ETH
           </div>
-          <button 
-            className="btn mr-2"
-            onClick={async() => {
-              const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
-              console.log(accounts)
-            }}
-          >
-            Enable Ethereum
-          </button>
           <button className="btn mr-2">Donate</button>
           <button className="btn">Withdraw</button>
         </div>
